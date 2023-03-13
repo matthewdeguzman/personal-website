46d55ceee304d6dc0578f5655051f8591ff69c7b
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2023-03-13 19:08:52 -0400

    added projects and project components

---
+++ components/project.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+export default function Project({
+  title,
+  description,
+  startTime,
+  endTime,
+  github,
+}) {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          {title}
+        </h2>
+
+        <div className="mt-6 gap-y-10 gap-x-6">
+          <div className="mt-4">
+            <div>
+              <h3 className="text-sm text-gray-700">
+                <span aria-hidden="true" className="absolute inset-0" />
+                {description}
+              </h3>
+              <Link href={github} className="mt-1 text-sm text-gray-500">
+                github
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,68 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
+import Head from "next/head";
+import Project from "../components/project";
+import { getSortedProjectsData } from "../lib/projects";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export function getStaticProps() {
+  const allProjectsData = getSortedProjectsData();
+  return {
+    props: {
+      allProjectsData,
+    },
+  };
+}
+export default function Home({ allProjectsData }) {
   return (
     <>
       <Head>
         <title>Personal Website</title>
-        <meta name="description" content="Matthew DeGuzman's personal website" />
+        <meta
+          name="description"
+          content="Matthew DeGuzman's personal website"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
+      {/* Introduction Section */}
       <div>
         <p className="text-5xl font-sans font-bold">
-          Hello. <br/>
+          Hello. <br />
           My name is Matthew DeGuzman.
         </p>
       </div>
+
+      {/* About me section */}
       <div className="flex justify-center border-2 border-black">
         <div className="flex-initial basis-1/3 text-center">
           <p>
-            I am a second-year student at the University of Florida majoring 
-            in Computer Science and Minoring in Mathematics. I am a software engineer
-            and have industry experience through two internships at Microsoft.
+            I am a second-year student at the University of Florida majoring in
+            Computer Science and Minoring in Mathematics. I am a software
+            engineer and have industry experience through two internships at
+            Microsoft.
           </p>
         </div>
       </div>
+
+      {/* Project Section */}
+      <div>
+        <h1 className="font-medium text-2xl">Projects</h1>
+        <div className="flex flex-wrap justify-evenly">
+          {allProjectsData.map(
+            ({ title, description, startTime, endTime, github }) => (
+              <div className="border-2 border-black flex-none w-64">
+                <Project
+                  key={github}
+                  title={title}
+                  description={description}
+                  startTime={startTime}
+                  endTime={endTime}
+                  github={github}
+                />
+              </div>
+            )
+          )}
+        </div>
+      </div>
+      {/* Contact form*/}
     </>
-  )
+  );
 }
