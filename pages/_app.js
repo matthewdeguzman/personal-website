9ff07baa88cb24648f34f65cc654dddbe6bfaebf
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2023-03-14 14:19:30 -0400

    made navbar persist

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
-import Navbar from '../components/navbar'
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-      <>
-        <Navbar/>
-        <Component {...pageProps} />
-      </>
-  )
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
