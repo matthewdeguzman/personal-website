e18bf59f763278e374770187f4d9f56cbde9452b
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2023-03-14 02:49:38 -0400

    fixed navbar alignment

--- components/navbar.js
+++ components/navbar.js
@@ -17,7 +17,7 @@ export default function Example() {
     <Disclosure as="nav" className="bg-white">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
@@ -31,7 +31,7 @@ export default function Example() {
                 </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="hidden sm:ml-6 sm:block">
+                <div className="hidden sm:ml-2 sm:block">
                   <a
                     key={navigation[0].name}
                     href={navigation[0].href}
